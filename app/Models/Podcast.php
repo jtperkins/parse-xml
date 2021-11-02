8a597e3c9e3c762e278a30b26881eb64dd1a3132
taylor <taylor.perkins@alt-tab-design.com>
2021-11-02 18:58:33 -0500

    add Podcast model and migration

---
+++ app/Models/Podcast.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Podcast extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'artwork_url',
+        'rss_feed_url',
+        'description',
+        'language',
+        'website_url'
+    ];
+}
---
+++ database/migrations/2021_11_02_234457_create_podcasts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePodcastsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('podcasts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('artwork_url');
+            $table->string('rss_feed_url');
+            $table->string('description');
+            $table->string('language')->default('en-us');
+            $table->string('website_url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('podcasts');
+    }
+}
