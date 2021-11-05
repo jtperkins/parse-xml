2121e925e5a9247d45ad6bb97a6a6a6d2d04c773
taylor <taylor.perkins@alt-tab-design.com>
2021-11-05 13:40:55 -0500

    update RssTest to use collections

--- tests/Feature/RssTest.php
+++ tests/Feature/RssTest.php
@@ -5,7 +5,6 @@ namespace Tests\Feature;
 use App\Classes\Rss;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
-use SimpleXMLElement;
 use Tests\TestCase;
 
 class RssTest extends TestCase
@@ -18,7 +17,7 @@ class RssTest extends TestCase
      */
     public function test_rss_parse()
     {
-        $test_rss_urls = [
+        $test_rss_urls = collect([
             'https://www.omnycontent.com/d/playlist/2b465d4a-14ee-4fbe-a3c2-ac46009a2d5a/b1907157-de93-4ea2-a952-ac700085150f/be1924e3-559d-4f7d-98e5-ac7000851521/podcast.rss',
             'https://nosleeppodcast.libsyn.com/rss',
             'https://www.omnycontent.com/d/playlist/aaea4e69-af51-495e-afc9-a9760146922b/43816ad6-9ef9-4bd5-9694-aadc001411b2/808b901f-5d31-4eb8-91a6-aadc001411c0/podcast.rss',
@@ -31,10 +30,11 @@ class RssTest extends TestCase
             'http://podcasts.subsplash.com/ec8de47/podcast.rss',
             'https://feeds.megaphone.fm/VMP5705694065',
             'https://feeds.simplecast.com/54nAGcIl'
-        ];
+        ]);
 
-        foreach ($test_rss_urls as $rss) {
-            Rss::Parse(@simplexml_load_file($rss) ?: null, $rss);
-        }
+        $test_rss_urls->each(function ($url) {
+            Rss::Parse(@simplexml_load_file($url) ?: null, $url);
+        });
+
     }
 }
