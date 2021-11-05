877280fdfbd964fdb296971211874da3dd3a5615
taylor <taylor.perkins@alt-tab-design.com>
2021-11-05 13:43:18 -0500

    cleaning up some Rss Parse things, improvements based on testing

--- app/Classes/Rss.php
+++ app/Classes/Rss.php
@@ -9,39 +9,42 @@ use SimpleXMLElement;
 
 class Rss
 {
-    public static function Parse(SimpleXMLElement $rss_object, string $rss_url)
+    public static function Parse(SimpleXMLElement $rss_object = null, string $rss_url)
     {
+        if (!$rss_object) return;
+
         if ($podcast = Podcast::firstWhere(['rss_feed_url' => $rss_url])) {
 
             $podcast->update([
+                'title' => isset($rss_object->channel->title) && $rss_object->channel->title ? $rss_object->channel->title : null,
                 'artwork_url' => isset($rss_object->channel->image->url) ? $rss_object->channel->image->url : null,
                 'description' => isset($rss_object->channel->description) ? $rss_object->channel->description : null,
                 'website_url' => isset($rss_object->channel->link) ? $rss_object->channel->link : null
             ]);
 
-            static::_parse_tems($rss_object, $podcast);
+            static::_parse_items($rss_object, $podcast);
 
         } else {
 
             $podcast = Podcast::create([
-                'title' => isset($rss_object->channel->title) ? $rss_object->channel->title : null,
+                'title' => isset($rss_object->channel->title) && $rss_object->channel->title ? $rss_object->channel->title : null,
                 'artwork_url' => isset($rss_object->channel->image->url) ? $rss_object->channel->image->url : null,
                 'rss_feed_url' => $rss_url,
                 'description' => isset($rss_object->channel->description) ? $rss_object->channel->description : null,
-                'language' => $rss_object->channel->language,
+                'language' => isset($rss_object->channel->language) && $rss_object->channel->language ? $rss_object->channel->language : null,
                 'website_url' => isset($rss_object->channel->link) ? $rss_object->channel->link : null
             ]);
 
-            static::_parse_tems($rss_object, $podcast);
+            static::_parse_items($rss_object, $podcast);
 
         }
     }
 
-    public static function _parse_tems(SimpleXMLElement $rss_object, Podcast $podcast)
+    public static function _parse_items(SimpleXMLElement $rss_object, Podcast $podcast)
     {
-        $episodes = [];
+        $episodes = collect();
 
-        if (count($rss_object->channel->item)) {
+        if ($rss_object->channel->item->count()) {
 
             foreach ($rss_object->channel->item as $item) {
 
@@ -49,21 +52,21 @@ class Rss
 
                     $now = Carbon::now();
 
-                    $episodes[] = [
-                        'title' => $item->title,
+                    $episodes->push([
+                        'title' => isset($item->title) && $item->title ? $item->title : null,
                         'audio_url' => isset($item->enclosure['url']) ? $item->enclosure['url'] : null,
                         'description' => isset($item->description) ? $item->description : null,
                         'episode_url' => isset($item->link) ? $item->link : null,
                         'podcast_id' => $podcast->id,
                         'created_at' => $now,
                         'updated_at' => $now
-                    ];
+                    ]);
 
                 }
 
             }
 
-            Episode::insert($episodes);
+            if ($episodes->count()) Episode::insert($episodes->toArray());
 
         }
         
